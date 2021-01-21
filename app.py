1e3a4d7924b76d182f7b538f29aadda9086aff0f
Fernando Wittmann <fernando.wittmann@gmail.com>
2021-01-21 19:04:09 -0200

    ..

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('primeiro_app.html')
+    return render_template('segundo_app.html')
+
+@app.route('/greet', methods=['POST'])
+def cumprimentar():
+    nome = [el for el in request.form.values()]
+    return render_template('segundo_app.html', text='Olá '+nome[0])
 
 if __name__=='__main__':
     app.run()
